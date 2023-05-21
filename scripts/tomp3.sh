86081e8999a650a156d53fa22155ef00117dafc7
hpompougnac <hpompougnac@debian-hugo.home>
2023-05-21 23:15:25 +0200

    Typo

--- scripts/tomp3.sh
+++ scripts/tomp3.sh
@@ -5,4 +5,7 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-parallel --bar --jobs 3 '(ffmpeg -i {} -qscale:a 0 {.}.mp3 2> /dev/null && rm {.}.flac)' ::: "$1"/*.flac
+parallel --bar \
+	 --jobs 3 \
+	 '(ffmpeg -i {} -qscale:a 0 {.}.mp3 2> /dev/null && rm {.}.flac)' \
+	 ::: "$1"/*.flac
