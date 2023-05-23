0ec01680db98bbc755e686d55386ac960261fa2a
hpompougnac <hpompougnac@debian-hugo.home>
2023-05-23 09:46:39 +0200

    Music correction

--- scripts/tom3u.sh
+++ scripts/tom3u.sh
@@ -5,4 +5,20 @@ if [[ $# -ne 2 ]] ; then
     exit 1
 fi
 
-find "$(realpath "$1")" -name *.flac | sort -R > "$2"
+numfiles=$(find "$1" -name "*.flac" | wc -l)
+i=0
+
+echo "#EXTM3U" > $2
+
+shopt -s globstar
+for f in "$1"/**/*.flac; do
+    track=$(ffprobe "$f" |& grep -i -m 1 "track" | cut -d ':' -f 2 | sed 's/^ //g')
+    title=$(ffprobe "$f" |& grep -i -m 1 "title" || echo ' : Unknown' | cut -d ':' -f 2 | sed 's/^ //g')
+    artist=$(ffprobe "$f" |& grep -i -m 1 "artist" || echo ' : Unknown' | cut -d ':' -f 2 | sed 's/^ //g')
+    echo "#EXTINF:$track, $artist - $title" >> $2
+    dname1="$(realpath "$f")"
+    echo "$dname1" >> $2
+    ((i=i+1))
+    vramsteg --label "$i/$numfiles" --min 0 --max $numfiles --current $i
+done
+vramsteg --remove
