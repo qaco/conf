0455acacde22aadcf787026dbdd4d32f7bc3883d
hpompougnac <hpompougnac@inria-Latitude-5520.inrialpes.fr>
2023-10-05 10:31:04 +0200

    Install script corrections

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ rm -f ~/.config/newsboat
 rm -f ~/.config/fontconfig
 rm -f ~/.local/share/rofi/themes
 rm -f ~/.config/picom
-rm -f ~/.config/tilix
 
 mkdir -p ~/.config/conky
 mkdir -p ~/.config/i3
@@ -22,14 +21,12 @@ mkdir -p ~/.config/newsboat
 mkdir -p ~/.config/fontconfig
 mkdir -p ~/.local/share/rofi/themes
 mkdir -p ~/.config/picom
-mkdir -p ~/.config/tilix
 
 ln conky/i3bar.conky.conf ~/.config/i3/i3bar.conky.conf
-ln conf/i3/config ~/.config/i3/config
-ln conf/i3/conky-i3bar ~/.config/i3/conky-i3bar
-ln conf/newsboat/config .config/newsboat/config
-ln conf/newsboat/urls .config/newsboat/urls
+ln i3/config ~/.config/i3/config
+ln i3/conky-i3bar ~/.config/i3/conky-i3bar
+ln newsboat/config .config/newsboat/config
+ln newsboat/urls .config/newsboat/urls
 ln -s conf/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
 ln rofi/themes/onedark.rasi ~/.local/share/rofi/themes/onedark.rasi
-ln conf/picom/picom.conf .config/picom/picom.conf
-ln -s conf/tilix/one-dark.json .config/tilix/one-dark.json
+ln picom/picom.conf ~/.config/picom/picom.conf
