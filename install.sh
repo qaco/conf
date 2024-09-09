78885721972164631b22c051cbe0d378b522e1a4
hpompougnac <hpompougnac@precision3571.home>
2024-09-09 21:06:17 +0200

    Corrections

--- install.sh
+++ install.sh
@@ -1,31 +1,28 @@
 #! /bin/bash
 
+# sudo apt install network-manager-gnome opam
+
 rm ~/.bashrc
 rm ~/.profile
 
 ln -s ~/conf/.bashrc ~/.bashrc
 ln -s ~/conf/.profile ~/.profile
 ln -s ~/conf/.emacs.d/init/.emacs ~/.emacs
-ln -s ~/conf/.emacs.d/ ~/.emacs.d
 mkdir ~/.saves
+touch ~/.paths
 
 rm -f ~/.config/rofi
 rm -f ~/.config/conky
-rm -f ~/.config/i3
-rm -f ~/.config/newsboat
-rm -f ~/.config/fontconfig
 rm -f ~/.local/share/rofi/themes
 rm -rf ~/.config/picom
 
 mkdir -p ~/.config/rofi
-mkdir -p ~/.config/conky
 mkdir -p ~/.config/i3
-mkdir -p ~/.config/newsboat
-mkdir -p ~/.config/fontconfig
 mkdir -p ~/.local/share/rofi/themes
 mkdir -p ~/.config/picom
 
 echo "include /home/hpompougnac/conf/i3/config" > ~/.config/i3/config
+cp ~/conf/i3/i3bar.conky.conf.template ~/conf/i3/i3bar.conky.conf
 ln ~/conf/i3/i3bar.conky.conf ~/.config/i3/i3bar.conky.conf
 ln ~/conf/i3/conky-i3bar ~/.config/i3/conky-i3bar
 ln ~/conf/rofi/themes/onedark.rasi ~/.local/share/rofi/themes/onedark.rasi
