e99a1e58c349b3927c643a82a4ba6eb3254d743e
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2024-12-21 14:30:41 +0000

    Update the driver file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,4 @@
-;; Packages
-
-(add-to-list
- 'package-archives
- '("melpa-stable" . "https://stable.melpa.org/packages/")
- t)
-;; (setq package-install-upgrade-built-in t)
-(package-initialize)
-
-(add-to-list 'display-buffer-alist
-             '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
-               (display-buffer-no-window)
-               (allow-no-window . t)))
-
-(setq package-archive-enable-alist nil)
-
-;; Environment variables
-
-(setq load-path (append load-path '("~/conf/.emacs.d/elisp")))
-(setq custom-file null-device)
 (setq load-path (append load-path '("~/conf/.emacs.d")))
-(setq backup-directory-alist `(("." . "~/.saves")))
 
 (require 'standalone)
-(require 'my-mode-line)
-(require 'mlir-mode)
-(require 'tablegen-mode)
-(require 'git)
-(require 'org-conf)
-(require 'console)
-(require 'ide)
 (require 'extra)
