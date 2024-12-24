9546a57479e69d3f1f7e605656cc6c901ab680b7
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2024-12-24 19:22:37 +0000

    Remove .emacs.d

--- .emacs.d/standalone.el
+++
@@ -1,7 +0,0 @@
-(setq load-path (append load-path '("~/conf/.emacs.d/init")))
-(setq load-path (append load-path '("~/conf/.emacs.d/major-modes")))
-
-(require 'standalone)
-(require 'my-mode-line)
-(require 'mlir-mode)
-(require 'tablegen-mode)
