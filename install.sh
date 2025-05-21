7b598b8157bfb01b2a5dc26575aebf6eb40e8bd2
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-05-21 07:03:08 +0200

    i3: spring cleaning

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@
 # terminal management
 rm ~/.bashrc
 rm ~/.profile
-rm ~/.zshrc
 ln -s ~/conf/.bashrc ~/.bashrc
-cp ~/conf/.zshrc ~/.zshrc
 ln -s ~/conf/.profile ~/.profile
 cp ~/conf/.bash_aliases ~/.bash_aliases
 touch ~/.paths
@@ -23,18 +21,12 @@ ln -s ~/conf/.emacs.d/init.el ~/.emacs
 mkdir ~/.saves
 
 # desktop environment management
-rm -f ~/.config/rofi
 rm -f ~/.config/conky
-rm -f ~/.local/share/rofi/themes
 rm -rf ~/.config/picom
-mkdir -p ~/.config/rofi
 mkdir -p ~/.config/i3
-mkdir -p ~/.local/share/rofi/themes
 mkdir -p ~/.config/picom
 echo "include /home/hpompougnac/conf/i3/config" > ~/.config/i3/config
 cp ~/conf/i3/i3bar.conky.conf.template ~/conf/i3/i3bar.conky.conf
 ln ~/conf/i3/i3bar.conky.conf ~/.config/i3/i3bar.conky.conf
 ln ~/conf/i3/conky-i3bar ~/.config/i3/conky-i3bar
-ln ~/conf/rofi/themes/onedark.rasi ~/.local/share/rofi/themes/onedark.rasi
-ln ~/conf/rofi/config.rasi ~/.config/rofi/config.rasi
 ln ~/conf/picom/picom.conf ~/.config/picom/picom.conf
